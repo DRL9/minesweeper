1a571f27982ff2ca033ffbb48744b8814fc35193
Yuanjun Zhong <zhuimengzyj@gmail.com>
2017-10-24 19:05:36 +0800

    style: change eslint config
    change indent size from 2 to 4
    always require semi
    
    Changes to be committed:
    	modified:   .editorconfig
    	modified:   .eslintrc.js
    	modified:   src/main.js
    	modified:   test/unit/index.js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import './css/init.css'
+import Vue from 'vue';
+import App from './App';
+import './css/init.css';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
-})
+    el: '#app',
+    template: '<App/>',
+    components: { App }
+});
